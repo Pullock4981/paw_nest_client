9fb2bc6a723c1f5b0f7152676e19e9bd30601024
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-08-17 15:15:14 +0600

    add modifyed home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,16 +5,47 @@ import CallToAction from './CallToAction';
 import AboutUs from './AboutUs';
 import Testimonials from './Testimonials';
 import HowItWorks from './HowItWorks';
+import FeaturedPets from './FeaturedPets';
+import RecentPets from './RecentPets';
+import AdoptionStories from './AdoptionStories';
+import Newsletter from './Newsletter';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
-            <PetCategories></PetCategories>
-            <CallToAction></CallToAction>
-            <AboutUs></AboutUs>
-            <Testimonials></Testimonials>
-            <HowItWorks></HowItWorks>
+            {/* Hero Section */}
+            <Banner />
+
+            {/* Category Highlights */}
+            <PetCategories />
+
+            {/* Featured Section */}
+            <FeaturedPets />
+
+            {/* Recent Listings */}
+            <RecentPets />
+
+            {/* Call To Action */}
+            <CallToAction />
+
+            {/* About Section */}
+            <AboutUs />
+
+            {/* Success / Adoption Stories */}
+            <AdoptionStories />
+
+            {/* Testimonials / Reviews */}
+            <Testimonials />
+
+            {/* How It Works */}
+            <HowItWorks />
+
+            {/* Newsletter / Updates */}
+            <Newsletter />
+
+            {/* Footer */}
+            <Footer />
         </div>
     );
 };
